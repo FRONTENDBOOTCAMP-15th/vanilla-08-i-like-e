cc0f0009756f6aca16571bc36913389db317aeed
jetin020822-afk <jetin020822@gmail.com>
2025-11-10 17:15:23 +0900

    Build: 프리티어 및 파일구조 변경

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         // 메인 페이지
-        index: 'index.html'
+        index: "index.html",
       },
     },
   },
-  appType: 'mpa', // fallback 사용안함
+  appType: "mpa", // fallback 사용안함
 });
